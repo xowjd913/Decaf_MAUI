a9e035f23befcb6b7e189027aeef8c90409b4abc
TJKim <xowjd913@gmail.com>
2024-09-07 14:19:58 +0900

    🎨 [UI] : 온보딩 재활용 CheckBox ControlTemplate로 별도 분리 작업 완료

---
+++ Decaf.Template/OnBoardingCheckBox/OnBoardingCheckBoxControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Decaf.Template;
+
+public partial class OnBoardingCheckBoxControl : ContentView
+{
+	public static readonly BindableProperty TextProperty
+		= BindableProperty.Create(nameof(Text), typeof(string), typeof(OnBoardingCheckBoxControl), default(string));
+
+	public string Text
+	{
+		get => (string)GetValue(TextProperty);
+		set => SetValue(TextProperty, value);
+	}
+
+	public static readonly BindableProperty IsCheckedProperty
+		= BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(OnBoardingCheckBoxControl), default(bool));
+
+	public bool IsChecked
+	{
+		get => (bool)GetValue(IsCheckedProperty);
+		set => SetValue(IsCheckedProperty, value);
+	}
+
+	public OnBoardingCheckBoxControl()
+		=> InitializeComponent();
+}
