4f7219dd6480c890a4b06cced99b673b0961f8b0
TJKim <xowjd913@gmail.com>
2024-09-21 15:36:08 +0900

    ✨[New] : 체크박스 1개만 클릭되도록 추가 & Collection view로 바인딩 되도록 추가

---
+++ Decaf.OnBoarding/Models/OnBoardingSurvey.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Decaf.OnBoarding.Models
+{
+	public class OnBoardingSurvey : BindableBase
+	{
+		private bool isChecked;
+		public bool IsChecked
+		{
+			get => isChecked;
+			set
+			{
+				isChecked = value;
+
+				if (value)
+					onActiveIsChecked?.Invoke(Text);
+
+				OnPropertyChanged(new(nameof(IsChecked)));
+			}
+		}
+
+		private string text;
+		public string Text
+		{
+			get => text;
+			set
+			{
+				text = value;
+				OnPropertyChanged(new(nameof(Text)));
+			}
+		}
+
+		private Action<string> onActiveIsChecked;
+
+		public OnBoardingSurvey(Action<string> onActiveIsChecked)
+		{
+			this.onActiveIsChecked = onActiveIsChecked;
+		}
+	}
+}
+
--- Decaf.OnBoarding/ViewModels/OnBoardingPageViewModel.cs
+++ Decaf.OnBoarding/ViewModels/OnBoardingPageViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows.Input;
 using Decaf.Base.MVVM;
+using Decaf.OnBoarding.Models;
 
 namespace Decaf.OnBoarding.ViewModels
 {
+
     public class OnBoardingPageViewModel : ViewModelBase
     {
+
+        #region [ Command ]
         public ICommand IsTappedToggleCommand { get; private set; }
+        #endregion
+
+        #region [ Properties ]
+        public ObservableCollection<OnBoardingSurvey> OnBoardingSurveys { get; private set; }
+        #endregion
 
         public OnBoardingPageViewModel(
                 INavigationService navigationService
@@ -19,9 +29,49 @@ namespace Decaf.OnBoarding.ViewModels
             IsTappedToggleCommand = new Command(() =>
             {
                 Debug.WriteLine($"[{nameof(OnBoardingPageViewModel)}] [L] : Tapped....");
-                
             });
+
+            CreateOnBoardingSurveys();
         }
+
+
+        #region [ Private methods ]
+        private void CreateOnBoardingSurveys()
+        {
+            void OnActiveIsChecked(string text)
+            {
+                Debug.WriteLine($"[L] : {text} CHECKED !!!!");
+
+                var otherSurveys = OnBoardingSurveys
+                    .Where(o => o.Text.Equals(text) == false)
+                    .ToList();
+
+                otherSurveys.ForEach(element =>
+                {
+                    element.IsChecked = false;
+                });
+            }
+
+            OnBoardingSurveys = new ObservableCollection<OnBoardingSurvey>()
+            {
+                new(OnActiveIsChecked)
+                {
+                    Text = "3잔 이상",
+                    IsChecked = false,
+                },
+                new(OnActiveIsChecked)
+                {
+                    Text = "1잔",
+                    IsChecked = false,
+                },
+                new(OnActiveIsChecked)
+                {
+                    Text = "아예 못 마심",
+                    IsChecked = false,
+                },
+            };
+        }
+        #endregion
     }
 }
 
